c323274a77505241f64a0124d665b80e4280508f
esekyere <EricSekyere@users.noreply.github.com>
2018-06-19 18:22:26 -0400

    removed default logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import logo from './logo.svg'
 import './App.css'
 import store from './store'
 import Articles from './components/Article/Articles'
@@ -11,10 +10,6 @@ class App extends Component {
     return (
       <Provider store={ store }>
         <div className="App">
-          {/*<header className="App-header">
-            <img src={ logo } className="App-logo" alt="logo"/>
-            <h1 className="App-title">NewsFeed Starter</h1>
-    </header>*/}
           <div className = "container">
             <Sources/>
             <Articles/>
